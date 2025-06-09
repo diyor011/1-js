153937b308f868aedd336f60cba665d39e50bbe6
diyor011 <104572938+diyor011@users.noreply.github.com>
2025-06-09 22:36:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log('Malumotlarim');
+
+// var student1 = 'Odilbek'
+// let student2 = 'Ekaboy'
+// const student3 = 'Soliha'
+
+
+
+
+
+
+
+
+// confirm ('Ismoil qizlaga yozma 😊')
+
+
+let name = prompt('Ismingizni Kiriting')
+let surname = prompt(' Familiyangizni Kiriting')
+let  Age = prompt ('Yoshingizni Kiriting')
+let job = prompt ( 'Yoqtirgan Faningizni Yozing')
+let center =prompt ('Yashaydigon Tumaningizni Kiriting')
+
+
+//  console.log(name);
+//  console.log(surname);
+//  console.log(Age);
+//  console.log(job);
+// console.log(center);
+
+
+ 
+ 
+console.log( 'Ism:' +  name );
+console.log('Familiya:' + surname);
+console.log('Yosh:' + Age);
+console.log('Yoqdirgan Faningiz:' + job);
+console.log('Yashash Manzili:' + center);
+
+alert ('Malumotlaringizni Kiritganingiz Uchun Tashakkur🎁')
+
+
